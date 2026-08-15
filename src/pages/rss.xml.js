bff64d7c0a6124efebf7120fe4d41f6773484aee
Александр Ивашков <psscream@MacBook-Pro-Aleksandr.local>
2026-08-15 23:12:47 +0300

    Change rss extension to js for SSR

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { site, articles } from '../data/site';
 
-export const prerender = true;
-
-export async function GET(context: any) {
+export async function GET(context) {
   const cases = await getCollection('cases');
   
   const caseItems = cases.map((c) => ({
